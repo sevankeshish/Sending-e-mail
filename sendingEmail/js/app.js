0dda6e19e3d74a40ddc775d64fc2d807b0912195
Sevan Keshish <35175803+sevankeshish@users.noreply.github.com>
2021-10-28 10:52:33 +0330

    validate Field

--- sendingEmail/js/app.js
+++ sendingEmail/js/app.js
@@ -1,12 +1,17 @@
 //variables
-const sendBtn = document.querySelector("#sendBtn")
-
+const sendBtn = document.querySelector("#sendBtn"),
+    email = document.querySelector("#email"),
+    subject = document.querySelector("#subject"),
+    message = document.querySelector("#message")
 
 //eventlisteners
 eventListeners()
 
 function eventListeners() {
     document.addEventListener("DOMContentLoaded", appInit)
+    email.addEventListener("blur" , validateField)
+    subject.addEventListener("blur" , validateField)
+    message.addEventListener("blur" , validateField)
 }
 
 
@@ -14,4 +19,19 @@ function eventListeners() {
 
 function appInit() {
     sendBtn.disabled = true;
+}
+
+function validateField(){
+    // console.log("true");
+    // console.log(this);
+
+    validateLength(this)
+}
+
+function validateLength(field){
+    if(field.value.length > 0){
+        console.log("true");
+    } else {
+        console.log("false");
+    }
 }
