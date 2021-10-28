ebaf82c26c3b856ef16b85dcf0b7900200f4d231
Sevan Keshish <35175803+sevankeshish@users.noreply.github.com>
2021-10-28 18:24:22 +0330

    submit email

--- sendingEmail/js/app.js
+++ sendingEmail/js/app.js
@@ -2,7 +2,9 @@
 const sendBtn = document.querySelector("#sendBtn"),
     email = document.querySelector("#email"),
     subject = document.querySelector("#subject"),
-    message = document.querySelector("#message")
+    message = document.querySelector("#message"),
+    resetBtn = document.querySelector("#resetBtn"),
+    form = document.querySelector("#email-form")
 
 //eventlisteners
 eventListeners()
@@ -12,6 +14,8 @@ function eventListeners() {
     email.addEventListener("blur", validateField)
     subject.addEventListener("blur", validateField)
     message.addEventListener("blur", validateField)
+    resetBtn.addEventListener("click", reset)
+    form.addEventListener('submit', submitEmail)
 }
 
 
@@ -19,6 +23,32 @@ function eventListeners() {
 
 function appInit() {
     sendBtn.disabled = true;
+}
+
+function submitEmail(e) {
+    e.preventDefault()
+
+    const spinner = document.getElementById("spinner")
+    spinner.style.display = "block"
+
+    const sendEmailImg = document.createElement("img")
+    // console.log(sendEmailImg);
+    sendEmailImg.src = "../img/mail.gif"
+    sendEmailImg.style.display = "block"
+    console.log(sendEmailImg);
+
+
+    setTimeout(() => {
+        spinner.style.display = "none"
+
+        const loaders = document.querySelector("#loaders")
+        loaders.appendChild(sendEmailImg)
+        setTimeout(() => {
+            resetForm()
+            sendEmailImg.remove()
+        }, 5000);
+
+    }, 3000);
 }
 
 function validateField() {
@@ -29,6 +59,14 @@ function validateField() {
     if (this.type === "email") {
         // console.log("email");
         validateEmail(this)
+    }
+
+    let error = document.querySelectorAll(".error")
+    //    console.log(error);
+    if (email.value !== " " && subject.value !== " " && message.value !== " ") {
+        if (error.length === 0) {
+            sendBtn.disabled = false
+        }
     }
 }
 
@@ -45,7 +83,7 @@ function validateLength(field) {
 }
 
 function validateEmail(field) {
-console.log(field.value);
+    // console.log(field.value);
     const emailText = field.value
     if (emailText.includes("@")) {
         field.style.borderBottomColor = "green"
@@ -55,5 +93,8 @@ console.log(field.value);
         field.style.borderBottomColor = "red"
         field.classList.add("error")
     }
+}
 
+function reset() {
+    form.reset()
 }
